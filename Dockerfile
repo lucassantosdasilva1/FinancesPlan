28b9ece97af5607c0a760f61b4ed696de4070633
Fabricio Araújo <fabriciofa@gmail.com>
2023-03-27 11:02:02 -0300

    :whale: configuração do docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
-FROM node:16
-WORKDIR /app
-COPY package*.json ./
-RUN npm install --only=production
+FROM node:16-alpine as modules-builder
+WORKDIR /usr/src/modules
+COPY package.json .
+RUN npm install
+
+FROM node:16-alpine
+WORKDIR /usr/src/app
+RUN npm install -g npm@9.6.2
+COPY --from=modules-builder /usr/src/modules .
 COPY . .
-CMD ["npm", "start"]
+RUN npx sequelize-cli db:migrate
+RUN npm run build
+ENTRYPOINT npm run start
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,10 +1,6 @@
-import { DataTypes, Model, Sequelize } from "sequelize";
-import { category, transactionType } from "../controller/@types";
-
-export const sequelizeConnection = new Sequelize("financesdb", "lucassantos", "senhadificil", {
-  host: "localhost",
-  dialect: "postgres",
-});
+import { DataTypes, Model } from 'sequelize';
+import { category, transactionType } from '../controller/@types';
+import { sequelize as sequelizeConnection } from '../database/Connection';
 
 export class Transaction extends Model {
   declare name: string;
@@ -26,17 +22,17 @@ Transaction.init(
       allowNull: false,
     },
     type: {
-      type: DataTypes.ENUM("income", "outcome"),
+      type: DataTypes.ENUM('income', 'outcome'),
       allowNull: false,
     },
     category: {
       type: DataTypes.ENUM(
-        "purchases",
-        "food",
-        "salary",
-        "car",
-        "leisure",
-        "studies"
+        'purchases',
+        'food',
+        'salary',
+        'car',
+        'leisure',
+        'studies',
       ),
       allowNull: false,
     },
@@ -51,60 +47,7 @@ Transaction.init(
   },
   {
     sequelize: sequelizeConnection,
-    modelName: "Transaction", // <- adicionado o nome do modelo aqui
-    tableName: "Transactions", // <- adicionado o nome da tabela aqui
-  }
+    modelName: 'Transaction', // <- adicionado o nome do modelo aqui
+    tableName: 'Transactions', // <- adicionado o nome da tabela aqui
+  },
 );
-
-
-// import { DataTypes, Model, Sequelize } from "sequelize";
-// import { category, transactionType } from "../controller/@types";
-// // import { sequelize } from "../database/Connection";
-// export const sequelizeConnection = new Sequelize("financesdb", "lucassantos", "senhadificil", {
-//   host: "localhost",
-//   dialect: "postgres",
-// });
-
-// export class Transaction extends Model {
-//   declare name: string;
-//   declare amount: number;
-//   declare type: transactionType;
-//   declare category: category;
-//   declare createdAt: Date;
-//   declare updatedAt: Date;
-// }
-
-// Transaction.init(
-//   {
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     amount: {
-//       type: DataTypes.DECIMAL,
-//       allowNull: false,
-//     },
-//     type: {
-//       type: DataTypes.ENUM("income", "outcome"),
-//       allowNull: false,
-//     },
-//     category: {
-//       type: DataTypes.ENUM(
-//         "purchases",
-//         "food",
-//         "salary",
-//         "car",
-//         "leisure",
-//         "studies"
-//       ),
-//       allowNull: false,
-//     },
-//     createdAt: {
-//       type: DataTypes.DATE,
-//       allowNull: false,
-//     },
-//     updatedAt: {
-//       type: DataTypes.DATE,
-//       allowNull: false,
-//     },
-//   }, { sequelize: sequelizeConnection, tableName: 'transaction' });
