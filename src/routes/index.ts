1b2cf5f4bff62ed9f82c2a33738d9f2fcedc51f7
lucas santos <lucassantosdasilva1@hotmail.com>
2023-03-27 13:44:39 -0300

    :books: swagger configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { TransactionController } from "../controller";
 import cors from "cors";
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 export const router = Router();
 
@@ -44,3 +46,30 @@ router.post("/newTransaction", newTransaction.create);
 router.put("/updateTransaction/:id", newTransaction.update);
 
 router.delete("/deleteTransaction/:id", newTransaction.delete);
+
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Finances Plan API",
+      version: "1.0.0",
+      description: "API para gerenciamento de finanças pessoais",
+      contact: {
+        name: "Lucas Santos",
+        url: "https://github.com/lucassantosdasilva1",
+        email: "lucas.santos@exemplo.com",
+      },
+    },
+    // servers: [
+    //   {
+    //     url: "http://localhost:3333",
+    //     description: "Servidor de desenvolvimento",
+    //   },
+    // ],
+  },
+  apis: ["./swagger.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from "express";
-const swaggerJSDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+
 import { router } from "./routes";
 
 const app = express();
@@ -17,29 +16,3 @@ app.use(baseRoute, router);
 app.listen(3333, () => {
   console.log("Server is running");
 });
-
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Finances Plan API",
-      version: "1.0.0",
-      description: "API para gerenciamento de finanças pessoais",
-      contact: {
-        name: "Lucas Santos",
-        url: "https://github.com/lucassantosdasilva1",
-        email: "lucas.santos@exemplo.com",
-      },
-    },
-    // servers: [
-    //   {
-    //     url: "http://localhost:3333",
-    //     description: "Servidor de desenvolvimento",
-    //   },
-    // ],
-  },
-  apis: ["./swagger.js"],
-};
-
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
